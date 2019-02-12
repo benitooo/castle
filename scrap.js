e526a6af1f7cd26ee5a2479735636d1482f44e2d
Benoit <benoit.feurgard@gmail.com>
2019-02-12 07:41:41 +0100

    liste des hotels

--- scrap.js
+++ scrap.js
@@ -1,8 +1,33 @@
 const request = require('request');
-const cherrio = require('cheerio');
-
-request('https://www.relaischateaux.com/fr/destinations/europe/france',(error,response,html)=>{
-    if(!error && response.statusCode==200){
-        console.log(html);
-    }
+const cheerio = require('cheerio');
+request('https://www.relaischateaux.com/fr/site-map/etablissements?fbclid=IwAR1N8kTL8UeswgVcnKkwcyVez23ky6IjrycQO9TqavWe5dOWhHx5GOG8Q-I',(error,response,body)=>
+{
+	if(!error && response.statusCode==200)
+	{
+        //console.log(html);
+	var $ = cheerio.load(body);
+	var tableau =[];
+	
+	$("#countryF").each(function(i)
+	{
+		if($(this).find("h3").text() === 'France')
+		{
+			$(this).find("li").each(function(j)
+			{
+				$(this).find("a").each(function(k)
+				{
+					if(k==0)
+					{
+					tableau.push($(this).filter("a").text().trim());
+					console.log($(this).filter("a").text().trim());
+					}
+				}
+				)}
+			)}
+	}
+	)}
 });
+
+
+
+
